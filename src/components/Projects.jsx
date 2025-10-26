54061e43e3ff05cfe0fc61620af3aad9a62a1abc
josiasmanzur02 <manzur.josias@gmail.com>
2025-10-26 16:32:28 -0600

    removal of accordion style on projects, hard to maintain and keep responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -87,50 +87,7 @@ const projects = [
 ];
 
 const Projects = () => {
-  useEffect(() => {
-    if (window.innerWidth <= 768) return;
-
-    const smoother = ScrollSmoother.create({
-      content: "#content",
-      wrapper: "#wrapper",
-      smooth: true,
-      effects: false,
-      normalizeScroll: true,
-    });
-
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".accordions",
-        pin: true,
-        start: "top top", 
-        end: "bottom top",
-        scrub: 1,
-        ease: "linear",
-      },
-    }); 
- 
-    tl.to(".accordion .project-card", {
-      height: 0,
-      paddingBottom: 0,
-      opacity: 0,
-      stagger: 0.5,
-    });
-
-    tl.to(
-      ".accordion",
-      {
-        marginBottom: -45,
-        stagger: 0.5,
-      },
-      "<"
-    );
-
-    return () => {
-      smoother.kill();
-      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
-    };
-  }, []);
-
+  
   return (
     <section className="projects-section" id="projects">
       <div className="accordions container">
